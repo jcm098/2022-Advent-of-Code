2f5a34756820c15e372191c9f8e7ea2bf470adfb
roomten <30985133+jcm098@users.noreply.github.com>
2022-12-03 15:17:50 +0000

    Days 2 and 3

---
+++ 02.py
@@ -0,0 +1,58 @@
+# Day 2 - https://adventofcode.com/2022/day/2
+def part1(data):
+  strategyGuide = {
+    #A and X are Rock, score 1
+    #B and Y are Paper, score 2
+    #C and Z are Scissors, score 3
+    #Lose, score 0
+    #Draw, score 3
+    #Win, score 6
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6
+  }
+  totalScore = 0
+  gamePlay = data.split('\n')
+
+  for aGame in gamePlay:
+    # print(strategyGuide.get(aGame))
+    totalScore += strategyGuide.get(aGame,0)
+    # print(totalScore)
+
+  print("Total points for this Rock/Paper/Scissors strategy is " + str(totalScore))
+  return totalScore
+
+def part2(data):
+  strategyGuide = {
+    #A is Rock, score 1
+    #B is Paper, score 2
+    #C is Scissors, score 3
+    #X is Lose, score 0
+    #Y is Draw, score 3
+    #Z is Win, score 6
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7
+  }
+  totalScore = 0
+  gamePlay = data.split('\n')
+
+  for aGame in gamePlay:
+    # print(strategyGuide.get(aGame))
+    totalScore += strategyGuide.get(aGame,0)
+    # print(totalScore)
+
+  print("Total points for this Rock/Paper/Scissors strategy is " + str(totalScore))
+  return totalScore
---
+++ 03.py
@@ -0,0 +1,54 @@
+#  Day 3 - https://adventofcode.com/2022/day/3
+
+def splitString(string):
+  first_half = string[0:len(string)//2]
+  second_half = string[len(string)//2:]
+  
+  return [first_half,second_half]
+
+def part1(data):
+  ruckSackItemTypes = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  # print(ruckSackItemTypes.index('A')+1)
+  ruckSacks = data.split('\n')
+  priorities = 0
+
+  for contents in ruckSacks:
+    # print(len(contents))
+    compartments = splitString(contents)
+    print(contents)
+    print(compartments[0] + ' - ' + compartments[1])
+    commonItems = set.intersection(set(compartments[0]), set(compartments[1]))
+    # print(''.join(commonStrings))
+    print(commonItems)
+    for item in commonItems:
+      priorities += ruckSackItemTypes.index(item) + 1
+      print(ruckSackItemTypes.index(item) + 1)
+    # print(compartments[1])
+  
+  return priorities
+
+
+def part2(data):
+  ruckSackItemTypes = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  # print(ruckSackItemTypes.index('A')+1)
+  ruckSacks = data.split('\n')
+  priorities = 0
+  teamSize = 3
+
+  for xLoop in range(int(len(ruckSacks)/teamSize)):
+    print(xLoop)
+    print(ruckSacks[xLoop*3])
+    print(ruckSacks[(xLoop*3)+1])
+    print(ruckSacks[(xLoop*3)+2])
+    commonItemsPart1 = set.intersection(set(ruckSacks[xLoop*3]), set(ruckSacks[(xLoop*3)+1]))
+    commonItemsPart2 = ''.join(commonItemsPart1)
+    commonItemsPart3 = set.intersection(set(commonItemsPart2), set(ruckSacks[(xLoop*3)+2]))
+    print(commonItemsPart3)
+
+    for item in commonItemsPart3:
+      priorities += ruckSackItemTypes.index(item) + 1
+      print(ruckSackItemTypes.index(item) + 1)
+
+  print(len(ruckSacks))
+  print(ruckSacks[0])
+  return priorities
